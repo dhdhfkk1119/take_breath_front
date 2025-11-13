33a2f30e6d901a5b0f440ae24b0e95a60f4f42aa
whwhwjddn123 <whwhwjddn123@gmail.com>
2025-11-13 15:20:15 +0900

    자동 로그인 수정 , 앱 실행 auth 검사 수정

--- lib/_core/utils/my_http.dart
+++ lib/_core/utils/my_http.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:take_breath/domain/member/services/auth_interceptor.dart';
+import 'package:take_breath/main.dart';
 
 const baseUrl = "http://192.168.0.87:8080/api";
 const imageLocalUrl = "http://10.0.2.2:8080/uploads/";
@@ -11,4 +12,4 @@ final dio = Dio(
     validateStatus: (status) => true, // 모든 상태 코드를 onResponse에서 처리
     listFormat: ListFormat.multi,
   ),
-)..interceptors.add(AuthInterceptor());
+)..interceptors.add(AuthInterceptor(navigatorKey: navigatorKey));
--- lib/domain/member/services/auth_interceptor.dart
+++ lib/domain/member/services/auth_interceptor.dart
@@ -1,8 +1,13 @@
 import 'package:dio/dio.dart';
-
+import 'package:flutter/material.dart';
+import 'package:take_breath/_core/utils/my_http.dart';
 import 'auth_storage.dart';
 
 class AuthInterceptor extends Interceptor {
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  AuthInterceptor({required this.navigatorKey});
+
   @override
   void onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
@@ -11,5 +16,61 @@ class AuthInterceptor extends Interceptor {
       options.headers['Authorization'] = 'Bearer $token';
     }
     super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    // 401 Unauthorized 처리
+    if (err.response?.statusCode == 401 &&
+        err.requestOptions.path != '/members/refresh') {
+      final refreshToken = await AuthStorage.getRefreshToken();
+
+      // refreshToken 없으면 바로 로그아웃
+      if (refreshToken == null) {
+        await _logout();
+        return handler.reject(err);
+      }
+
+      try {
+        // 1. refreshToken으로 새 accessToken 요청
+        final response = await dio.post(
+          '/members/refresh',
+          data: {'refreshToken': refreshToken},
+        );
+
+        final newAccessToken = response.data['accessToken'];
+        if (newAccessToken == null) {
+          // 갱신 실패 시 로그아웃
+          await _logout();
+          return handler.reject(err);
+        }
+
+        // 2. 새 accessToken 저장
+        await AuthStorage.saveTokens(newAccessToken, refreshToken);
+
+        // 3. 원래 요청 재시도
+        final requestOptions = err.requestOptions
+          ..headers['Authorization'] = 'Bearer $newAccessToken';
+
+        final retryResponse = await dio.fetch(requestOptions);
+        return handler.resolve(retryResponse);
+      } on DioException catch (_) {
+        // 갱신 실패 시 로그아웃
+        await _logout();
+        return handler.reject(err);
+      }
+    }
+
+    super.onError(err, handler);
+  }
+
+  Future<void> _logout() async {
+    await AuthStorage.clear();
+
+    // navigatorKey로 SocialPage 이동
+    navigatorKey.currentState?.pushNamedAndRemoveUntil(
+      '/social_page',
+      (route) => false,
+    );
   }
 }
